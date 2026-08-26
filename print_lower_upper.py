b59c11935edb492f779d264a11a2fd6c64ab56a6
Ryuugo-idea24 <mcz22309@ideacon.co.jp>
2026-08-26 20:16:56 +0900

    小文字大文字変換、最初の文字だけ大文字化など、表示方法を追加

---
+++ print_lower_upper.py
@@ -0,0 +1,13 @@
+
+def main():
+    str1 = "my precious time"
+    str2 = "MY PRECIOUS TIME"
+    print(str1.upper())
+    print(str2.lower())
+    print(str1.capitalize())
+    print(str2.capitalize())
+    print(str1.title())
+    print(str2.title())
+
+if __name__ == "__main__":
+    main()
